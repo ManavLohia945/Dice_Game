9fb851004b7703001ba4bf666041e387868c9d15
Manav Lohia <96055639+ManavLohia945@users.noreply.github.com>
2022-12-15 11:49:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-var randomNumber1 = Math.random() * 6;
-randomNumber1 = Math.floor(randomNumber1 + 1);
+var randomNumber1 = Math.random() * 6; //Math.random gives random number between 0 and 0.99 so if we multiply it with 6 it will give random no between 0 to 0.599.
+randomNumber1 = Math.floor(randomNumber1 + 1);//and when we do Math.floor() on that decimal no between 0 to 0.599 we get intger number from range 0 to 5.
 
 var randomNumber2 = Math.random() * 6;
 randomNumber2 = Math.floor(randomNumber2 + 1);
 
 var randomImg1 = "images/dice" + randomNumber1 + ".png";
 var randomImg2 = "images/dice" + randomNumber2 + ".png";
-document.querySelector(".img1").setAttribute("src",randomImg1);
+
+document.querySelector(".img1").setAttribute("src",randomImg1); //we want to change the img of dice on every refresh so we change the src attribute using setAttribute method of js.
 document.querySelector(".img2").setAttribute("src",randomImg2);
 
 if (randomNumber1 > randomNumber2) {
